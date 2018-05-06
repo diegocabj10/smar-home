0788d74d4cbb0342ce4f367f3053ab11e1beecbc
coliveda <coliveda@gmail.com>
2018-05-06 12:22:23 -0300

    select

--- WebApplication1/Scripts/ConfiguracionController.js
+++ WebApplication1/Scripts/ConfiguracionController.js
@@ -6,11 +6,22 @@ var myApp = angular.module('myApp', ['ui.bootstrap']);
 
 myApp.controller('myCtrl', ['$scope', '$http', '$interval',
     function EventosController($scope, $http, $interval) {
-
         var vm = this;
-
         vm.Controller = window.location.pathname + "/";
         ///**FUNCIONES**///
+
+        //vm.arduinos = [{ id:1, valor: "arduino1" }, { id: 2, valor: "arduino2" }];
+        vm.arduinos = function () {
+                $interval(function () {
+                    $http.post('/Configuracion/arduinos')
+                        .then(
+                        function (successResponse) {
+                            vm.arduinos = successResponse.data.Lista;
+                        }
+                        );
+                }, 1000, 100
+                );
+        };
 
         vm.Buscar = function () {
             $interval(function () {
